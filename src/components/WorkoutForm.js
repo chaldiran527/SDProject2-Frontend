e18cc61b4807a180c6da05d8a62c57044a7e163d
chaldiran527 <90536610+chaldiran527@users.noreply.github.com>
2024-06-17 08:00:35 -0600

    Update WorkoutForm.js

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -11,7 +11,7 @@ const WorkoutForm = () => {
 
         const workout = {title,load,reps}
 
-        const response = await fetch('/api/workouts',{
+        const response = await fetch('https://sdproject2-api.onrender.com/api/workouts',{
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
